8e6cb21c7c2f870a6120cd937686c6803dda44f3
Alesya Terlikova <micfisto@gmail.com>
2026-04-07 23:52:29 +0300

    feat(api): add user authentication endpoint and database connection
    Implement new `/auth` endpoint for user login and update database connection handling in `db.py`. Update report.md

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,32 @@
 from fastapi import FastAPI
+from sqlalchemy import text
+from db import engine
 
-app = FastAPI()
+app = FastAPI(
+    title="Geo Quiz API",
+    description="Backend для географической викторины",
+    version="1.0.0"
+)
+
 
 @app.get("/")
-async def root():
-    return {"message": "Hello, backend!"}
+def root():
+    return {"message": "Geo Quiz API is running"}
+
+
+@app.get("/db-test")
+def test_db():
+    try:
+        with engine.connect() as connection:
+            result = connection.execute(text("SELECT 1"))
+            value = result.scalar()
+
+            return {
+                "message": "Подключение к базе успешно",
+                "result": value
+            }
+    except Exception as e:
+        return {
+            "message": "Ошибка подключения к бд",
+            "error": str(e)
+        }
